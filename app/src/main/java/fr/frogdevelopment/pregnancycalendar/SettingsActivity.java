56c87ae5052004cbe778fdc359e41bd8e52fd92f
Le Gall Benoit <legall.benoit@gmail.com>
2017-01-07 17:14:12 +0100

    code review

--- app/src/main/java/fr/frogdevelopment/pregnancycalendar/SettingsActivity.java
+++ app/src/main/java/fr/frogdevelopment/pregnancycalendar/SettingsActivity.java
@@ -2,53 +2,72 @@ package fr.frogdevelopment.pregnancycalendar;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
+import static fr.frogdevelopment.pregnancycalendar.PregnancyUtils.KEY_DAYS_TO_FECUNDATION;
+import static fr.frogdevelopment.pregnancycalendar.PregnancyUtils.KEY_GESTATION_MAX;
+import static fr.frogdevelopment.pregnancycalendar.PregnancyUtils.KEY_GESTATION_MIN;
+
 public class SettingsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayShowHomeEnabled(true);
-        }
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayShowHomeEnabled(true);
+		}
 
-        getFragmentManager()
-                .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
-    }
+		getFragmentManager()
+				.beginTransaction()
+				.replace(android.R.id.content, new SettingsFragment())
+				.commit();
+	}
 
-    static public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
-        @Override
-        public void onCreate(final Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-        }
+	static public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+		@Override
+		public void onCreate(final Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preferences);
 
-        @Override
-        public void onResume() {
-            super.onResume();
-            getPreferenceScreen()
-                    .getSharedPreferences()
-                    .registerOnSharedPreferenceChangeListener(this);
-        }
+			EditTextPreference preferenceMin = (EditTextPreference) findPreference(KEY_GESTATION_MIN);
+			preferenceMin.setSummary(preferenceMin.getText());
 
-        @Override
-        public void onPause() {
-            super.onPause();
-            getPreferenceScreen()
-                    .getSharedPreferences()
-                    .unregisterOnSharedPreferenceChangeListener(this);
-        }
+			EditTextPreference preferenceMax = (EditTextPreference) findPreference(KEY_GESTATION_MAX);
+			preferenceMax.setSummary(preferenceMax.getText());
 
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-//fixme update info view ?
-        }
-    }
+			EditTextPreference preferenceDays = (EditTextPreference) findPreference(KEY_DAYS_TO_FECUNDATION);
+			preferenceDays.setSummary(preferenceDays.getText());
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+			getPreferenceScreen()
+					.getSharedPreferences()
+					.registerOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onPause() {
+			super.onPause();
+			getPreferenceScreen()
+					.getSharedPreferences()
+					.unregisterOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+			// update info view
+			Preference preference = findPreference(key);
+			if (preference instanceof EditTextPreference) {
+				preference.setSummary(((EditTextPreference) preference).getText());
+			}
+		}
+	}
 }
