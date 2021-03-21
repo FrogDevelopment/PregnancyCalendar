584c3f2406f9ab4f13fb8fde4c6aeff12bab57dc
Le Gall Benoit <legall.benoit@gmail.com>
2021-03-21 21:48:18 +0100

    fix: column definition

--- app/src/main/java/fr/frogdevelopment/pregnancycalendar/data/Contraction.kt
+++ app/src/main/java/fr/frogdevelopment/pregnancycalendar/data/Contraction.kt
@@ -1,18 +1,23 @@
 package fr.frogdevelopment.pregnancycalendar.data
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import org.threeten.bp.ZonedDateTime
 
-@Entity
+@Entity(tableName = "CONTRACTION")
 class Contraction {
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     var id: Int? = null
-    @ColumnInfo(name = "datetime")
+
+    @NonNull
+    @ColumnInfo(name = "DATETIME")
     var dateTime: ZonedDateTime? = null
-    @ColumnInfo
+
+    @NonNull
+    @ColumnInfo(name = "DURATION")
     var duration: Long? = null
 }
