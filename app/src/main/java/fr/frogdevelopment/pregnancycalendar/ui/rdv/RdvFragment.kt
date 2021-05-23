d868d753e4c3b6a121fe559a3975ff2541de833c
Le Gall Benoit <legall.benoit@gmail.com>
2021-05-23 22:43:53 +0200

    feat(rdv): calculate required (french) rdv for pregnancy

--- app/src/main/java/fr/frogdevelopment/pregnancycalendar/ui/rdv/RdvFragment.kt
+++ app/src/main/java/fr/frogdevelopment/pregnancycalendar/ui/rdv/RdvFragment.kt
@@ -1,4 +1,4 @@
-@file:Suppress("unused", "UNUSED_VARIABLE")
+@file:Suppress("unused", "UNUSED_VARIABLE", "SpellCheckingInspection")
 
 package fr.frogdevelopment.pregnancycalendar.ui.rdv
 
@@ -34,36 +34,54 @@ class RdvFragment : Fragment() {
     fun calculateRdv(amenorrheaDate: LocalDate) {
         println("Date Aménorrhée $amenorrheaDate")
 
+        println("***** RDV MEDICAUX")
         // 1er trimestre
-        val consultationPrenatale1From = amenorrheaDate.plusWeeks(8).minusDays(1)
-        val consultationPrenatale1To = amenorrheaDate.plusWeeks(12).minusDays(1)
-        println("1ère consultation prénatale entre $consultationPrenatale1From (8 SA) et $consultationPrenatale1To(12 SA)")
+        val consultationPrenatale1From = amenorrheaDate.plusWeeks(8)
+        val consultationPrenatale1To = amenorrheaDate.plusWeeks(12)
+        println("1ère consultation prénatale entre $consultationPrenatale1From (8 SA) et $consultationPrenatale1To (12 SA)")
 
-        val echo1 = amenorrheaDate.plusWeeks(11).plusDays(1)
-        println("Échographie 1 aux environ du $echo1 (12 SA)")
+        val echo1From = amenorrheaDate.plusWeeks(11)
+        val echo1To = amenorrheaDate.plusWeeks(13)
+        println("Échographie 1 (12 SA) entre $echo1From et $echo1To")
 
         // 2d trimestre
         val consultationPrenatale2 = calculateRdvMonthly(amenorrheaDate, 4)
         val consultationPrenatale3 = calculateRdvMonthly(amenorrheaDate, 5)
 
-        val echo2 = amenorrheaDate.plusWeeks(21).plusDays(1)
-        println("Échographie 2 aux environ du $echo2 (22 SA)")
+        val echo2From = amenorrheaDate.plusWeeks(21)
+        val echo2To = amenorrheaDate.plusWeeks(23)
+        println("Échographie 2 (22 SA) entre $echo2From et $echo2To")
 
         val consultationPrenatale4 = calculateRdvMonthly(amenorrheaDate, 6)
 
         // 3ème trimestre
         val consultationPrenatale5 = calculateRdvMonthly(amenorrheaDate, 7)
 
-        val echo3 = amenorrheaDate.plusWeeks(31).plusDays(1)
-        println("Échographie 3 aux environ du $echo3 (32 SA)")
+        val echo3From = amenorrheaDate.plusWeeks(31)
+        val echo3To = amenorrheaDate.plusWeeks(33)
+        println("Échographie 3 (32 SA) entre $echo3From et $echo3To")
 
         val consultationPrenatale6 = calculateRdvMonthly(amenorrheaDate, 8)
         val consultationPrenatale7 = calculateRdvMonthly(amenorrheaDate, 9)
+
+        println("***** RDV ADMINISTRATIFS")
+        val declarationGrossesse = amenorrheaDate.plusWeeks(15)
+        println("Déclaration grossesse CAF/CPAM (15 SA) avant le $declarationGrossesse")
+
+        val preparationAccouchementFrom = amenorrheaDate.plusWeeks(25)
+        val preparationAccouchementTo = amenorrheaDate.plusWeeks(35)
+        println("8 séances de préparation à l'accouchement entre le $preparationAccouchementFrom et $preparationAccouchementTo")
+
+        val maternite = amenorrheaDate.plusWeeks(28)
+        println("S'inscrire à la maternité avant le $maternite")
+
+        val anesthesiste = amenorrheaDate.plusWeeks(33)
+        println("Consultation chez l'anesthésiste avant le $anesthesiste")
     }
 
     private fun calculateRdvMonthly(amenorrheaDate: LocalDate, month: Long): LocalDate {
-        val from = amenorrheaDate.plusMonths(month - 1)
-        println("Consultation ${month}ème mois à partir de $from")
+        val from = amenorrheaDate.plusMonths(month - 1).plusDays(1)
+        println("Consultation ${month}ème mois à partir du $from")
         return from
     }
 
